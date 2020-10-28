05d01e2585d276782fbb73b01a084c019294068b
CHANDRA SEKHAR CHAVALI <chandrachavali@hotmail.com>
2020-10-28 16:37:29 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JenkinsIntegration.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
